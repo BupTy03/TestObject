f475a056edb1384931b9a3390bb33880b22a7894
Anton <anton.lagosha.99@mail.ru>
2019-11-02 21:38:12 +0500

    livingObjectsCount() method added

--- TestObject/TestObject.hpp
+++ TestObject/TestObject.hpp
@@ -2,110 +2,118 @@
 #ifndef TEST_OBJECT_HPP
 #define TEST_OBJECT_HPP
 
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
+
 
 namespace my
 {
-	class TestObject
-	{
-	public:
-		TestObject() : id{ ++counter }
+	struct TestObject {
+		static int livingObjectsCount() { return living_objects_count_; }
+
+		TestObject() : id_{ ++counter_ }
 		{
-			std::cout << "[object " << id << "] " << "TestObject::TestObject() (default constructor)" << std::endl;
-			status = "constructed with default constructor";
-			history.push_back(status);
+			std::cout << "[object " << id_ << "] " << "TestObject::TestObject() (default constructor)" << std::endl;
+			status_ = "constructed with default constructor";
+			history_.push_back(status_);
+			++living_objects_count_;
 		}
 
-		TestObject(int v) : id{ ++counter }, val{ v }
+		TestObject(int v) : id_{ ++counter_ }, val_{ v }
 		{
-			std::cout << "[object " << id << "] " << "TestObject::TestObject() (constructor with value)" << std::endl;
-			status = "constructed with value ";
-			status += std::to_string(v);
-			history.push_back(status);
+			std::cout << "[object " << id_ << "] " << "TestObject::TestObject() (constructor with value)" << std::endl;
+			status_ = "constructed with value ";
+			status_ += std::to_string(v);
+			history_.push_back(status_);
+			++living_objects_count_;
 		}
 
-		TestObject(const TestObject& other) : id{ ++counter }, parent_id{ other.id }, val{ other.val }
+		TestObject(const TestObject& other) : id_{ ++counter_ }, parent_id_{ other.id_ }, val_{ other.val_ }
 		{
-			std::cout << "[object " << id << "] " << "TestObject::TestObject(const TestObject& other) (copy constructor)" << std::endl;
-			status = "constructed with copy constructor from object with id: ";
-			status += std::to_string(other.id);
-			history.push_back(status);
+			std::cout << "[object " << id_ << "] " << "TestObject::TestObject(const TestObject& other) (copy constructor)" << std::endl;
+			status_ = "constructed with copy constructor from object with id: ";
+			status_ += std::to_string(other.id_);
+			history_.push_back(status_);
+			++living_objects_count_;
 		}
 		TestObject& operator=(const TestObject& other)
 		{
 			if (this == &other) return *this;
-			std::cout << "[object " << id << "] " << "TestObject& TestObject::operator=(const TestObject& other) (copy assignment operator)" << std::endl;
-			status = "parameters of this object is assigned with parameters of object with id: ";
-			status += std::to_string(other.id);
-			history.push_back(status);
-			val = other.val;
+			std::cout << "[object " << id_ << "] " << "TestObject& TestObject::operator=(const TestObject& other) (copy assignment operator)" << std::endl;
+			status_ = "parameters of this object is assigned with parameters of object with id: ";
+			status_ += std::to_string(other.id_);
+			history_.push_back(status_);
+			val_ = other.val_;
 			return *this;
 		}
 
-		TestObject(TestObject&& other) : id{ ++counter }, parent_id{ other.id }
+		TestObject(TestObject&& other) noexcept : id_{ ++counter_ }, parent_id_{ other.id_ }
 		{
-			std::cout << "[object " << id << "] " << "TestObject::TestObject(TestObject&& other) (move constructor)" << std::endl;
-			status = "constructed with move constructor from object with id: ";
-			status += std::to_string(other.id);
-			other.status = "is moved to new object with id: ";
-			other.status += std::to_string(id);
-			other.history.push_back(other.status);
-			val = other.val;
-			other.val = 0;
-			history.push_back(status);
+			std::cout << "[object " << id_ << "] " << "TestObject::TestObject(TestObject&& other) (move constructor)" << std::endl;
+			status_ = "constructed with move constructor from object with id: ";
+			status_ += std::to_string(other.id_);
+			other.status_ = "is moved to new object with id: ";
+			other.status_ += std::to_string(id_);
+			other.history_.push_back(other.status_);
+			val_ = other.val_;
+			other.val_ = 0;
+			history_.push_back(status_);
+			++living_objects_count_;
 		}
-		TestObject& operator=(TestObject&& other)
+		TestObject& operator=(TestObject&& other) noexcept
 		{
 			if (this == &other) return *this;
 
 			std::cout << "TestObject& TestObject::operator=(TestObject&& other) (move assignment operator)" << std::endl;
-			other.status = "this object is moved to object with id: ";
-			other.status += std::to_string(this->id);
-			other.history.push_back(other.status);
-			history.push_back(status);
-			val = other.val;
-			other.val = 0;
+			other.status_ = "this object is moved to object with id: ";
+			other.status_ += std::to_string(this->id_);
+			other.history_.push_back(other.status_);
+			history_.push_back(status_);
+			val_ = other.val_;
+			other.val_ = 0;
 			return *this;
 		}
 
 		virtual ~TestObject()
 		{
-			std::cout << "[object " << id << "] " << "virtual TestObject::~TestObject() (destructor)" << std::endl;
-			status = "this object is destructed";
-			history.push_back(status);
+			std::cout << "[object " << id_ << "] " << "virtual TestObject::~TestObject() (destructor)" << std::endl;
+			status_ = "this object is destructed";
+			history_.push_back(status_);
+			--living_objects_count_;
 		}
 
 		friend std::ostream& operator<<(std::ostream& os, const TestObject& obj)
 		{
-			os << "TestObject{id: " << obj.id << ", val: " << obj.val << " }"; 
+			os << "TestObject{ id: " << obj.id_ << ", val: " << obj.val_ << " }";
 			return os;
 		}
 
-		inline bool operator==(const TestObject& other) const noexcept { return val == other.val; }
-		inline bool operator!=(const TestObject& other) const noexcept { return val != other.val; }
-		inline bool operator< (const TestObject& other) const noexcept { return val <  other.val; }
-		inline bool operator> (const TestObject& other) const noexcept { return val >  other.val; }
-		inline bool operator<=(const TestObject& other) const noexcept { return val <= other.val; }
-		inline bool operator>=(const TestObject& other) const noexcept { return val >= other.val; }
+		inline bool operator==(const TestObject& other) const noexcept { return val_ == other.val_; }
+		inline bool operator!=(const TestObject& other) const noexcept { return val_ != other.val_; }
+		inline bool operator< (const TestObject& other) const noexcept { return val_ < other.val_; }
+		inline bool operator> (const TestObject& other) const noexcept { return val_ > other.val_; }
+		inline bool operator<=(const TestObject& other) const noexcept { return val_ <= other.val_; }
+		inline bool operator>=(const TestObject& other) const noexcept { return val_ >= other.val_; }
 
-		inline const std::vector<std::string>& getHistory() const noexcept { return history; }
-		inline const std::string& getStatus() const noexcept { return status; }
-		inline int getId() const noexcept { return id; }
-		inline int getParentId() const noexcept { return parent_id; }
-		inline int getValue() const noexcept { return val; }
+		inline const std::vector<std::string>& getHistory() const noexcept { return history_; }
+		inline const std::string& getStatus() const noexcept { return status_; }
+		inline int getId() const noexcept { return id_; }
+		inline int getParentId() const noexcept { return parent_id_; }
+		inline int getValue() const noexcept { return val_; }
 
-	protected:
-		static int counter;
-		int id{};
-		int parent_id{};
-		int val{};
-		std::string status;
-		std::vector<std::string> history;
+	private:
+		static int living_objects_count_;
+		static std::size_t counter_;
+		std::size_t id_ = 0;
+		std::size_t parent_id_ = 0;
+		int val_ = 0;
+		std::string status_;
+		std::vector<std::string> history_;
 	};
 
-	int TestObject::counter = 0;
+	int TestObject::living_objects_count_ = 0;
+	std::size_t TestObject::counter_ = 0;
 }
 
 #endif // TEST_OBJECT_HPP
